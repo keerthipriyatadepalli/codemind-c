e28281fbb7430ec45a3f79a0e7a6d1790b995d75
keerthipriyatadepalli <120557161+keerthipriyatadepalli@users.noreply.github.com>
2023-07-03 15:20:43 +0530

    #include<stdio.h> int main() {     float a,b,average;    //printf("enter the a,b values");     scanf("%f%f",&a,&b);     average=(a+b)/2;     printf("%.4f",average);     return 0; }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,average;
+   //printf("enter the a,b values");
+    scanf("%f%f",&a,&b);
+    average=(a+b)/2;
+    printf("%.4f",average);
+    return 0;
+}
